e09696b81aff14f58e46592d9f05accd19775de8
oahednoorforhad <oahednoorforhad@gmail.com>
2024-11-05 05:53:11 +0600

    login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,34 @@
-import NextAuth from "next-auth/next"
-import CredentialProvider from "next-auth/providers/credentials"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import NextAuth from "next-auth/next";
+import CredentialProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-    session: {
-        strategy: 'jwt',
-        maxAge: 30 * 24 * 60 * 60
-    },
-    providers: [
-        CredentialProvider({
-            credentials: {
-                username: {},
-                password: {}
-            
-            },
-            async authorize(credentials) {
-                return true;
-            }
-        })
-    ],
-    debug: true,
-    callbacks: {},
-    pages: {
-        signIn: '/login'
-    }
-})
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  providers: [
+    CredentialProvider({
+      credentials: {
+        username: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "1",
+          name: "Admin User",
+          email: "admin@example.com",
+        };
+        console.log("Authorized user:", user);
+        return user;
+      },
+    }),
+  ],
+  //debug: true,
+  callbacks: {},
+  pages: {
+    signIn: "/login",
+  },
+});
 
-export {handler as Get, handler as POST}
+export { handler as Get, handler as POST };
